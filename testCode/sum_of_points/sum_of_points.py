98120260cf592f923eac10067f4e8fe071a8badf
Maren Turbow <mturbow00@gmail.com>
2015-12-20 20:26:38 -0500

    Asks for elliptic curve input and prints equation

--- testCode/sum_of_points/sum_of_points.py
+++ testCode/sum_of_points/sum_of_points.py
@@ -1,5 +1,17 @@
 #ask for input of an elliptic curve in the form y^2+a_1(xy)+a_3y=x^3+a_2(x^2)+a_4x+a_6 
 
+a1 = raw_input("Enter a1:")
+a2 = raw_input("Enter a2:")
+a3 = raw_input("Enter a3:")
+a4 = raw_input("Enter a4:")
+a6 = raw_input("Enter a6:")
+
+print "Hello"
+print "Elliptic Curve defined by y^2 + " + str(a1) +"xy + " + str(a3) +"y = x^3 + " + str(a2) + "x^2 + " + str(a4) +"x + " + str(a6)
+
+# + str(a2) +"x^2 + " + str(a4) +"x" + str(a6)
+ 
+
 #ask to input 2 points on the curve
 
 
