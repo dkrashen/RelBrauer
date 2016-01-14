2c15289add54f1afc4b5577a66872cda0f23f760
Maren Turbow <mturbow00@gmail.com>
2016-01-14 14:55:47 -0500

    Defines function to add two points.  Includes test curve and points.

--- testCode/sum_of_points/sum_of_points.py
+++ testCode/sum_of_points/sum_of_points.py
@@ -1,37 +1,26 @@
-#ask for input of an elliptic curve in the form y^2+a_1(xy)+a_3y=x^3+a_2(x^2)+a_4x+a_6 
-
-a1 = raw_input("Enter a1:")
-a2 = raw_input("Enter a2:")
-a3 = raw_input("Enter a3:")
-a4 = raw_input("Enter a4:")
-a6 = raw_input("Enter a6:")
-
-print "Hello"
-print "Elliptic Curve defined by y^2 + " + str(a1) +"xy + " + str(a3) +"y = x^3 + " + str(a2) + "x^2 + " + str(a4) +"x + " + str(a6)
-
-# + str(a2) +"x^2 + " + str(a4) +"x" + str(a6)
- 
-
-#ask to input 2 points on the curve
 
 
-#output the sum of the 2 points
+# This function will add two points on an ellipctic curve
 
 
 # sample input
 
-p1 = something
-p2 = something else
+E = EllipticCurve('389a1')
 
-function add_points(p1, p2)
+P = E(-1,1)
+Q = E(0,-1)
 
 
-return ...
+#defin a function to add the two points
+
+def add_points(P, Q):
+
+	return P+Q
 
 
 #####
 ##testing area
 
-print add_points(p1, p2)
+print add_points(P, Q)
 
 
